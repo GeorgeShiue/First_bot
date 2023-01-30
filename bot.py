5d4b0b17306456d3a4fc0eaae50327a2019733f8
GeorgeShiue <georgeshiue1130@gmail.com>
2023-01-30 18:02:08 +0800

    send file or url

--- bot.py
+++ bot.py
@@ -1,6 +1,7 @@
 import discord
 from discord.ext import commands
 import json
+import random
 
 with open('setting.json', mode = 'r', encoding = 'utf8') as jfile:
     jdata = json.load(jfile) #開啟json設定檔並讀取裡面的所有資料
@@ -24,5 +25,17 @@ async def on_member_remove(member):
 @bot.command() #指令
 async def ping(ctx): #使用者使用ping指令時，ctx便會包含使用者名稱、id、所在伺服器、所在頻道等訊息
     await ctx.send(f'{round(bot.latency*1000, 2)}ms') #依據ctx所提供的訊息傳送ping值(bot.latency)
+
+@bot.command() #指令
+async def random_pic(ctx): 
+    random_pic_path = random.choice(jdata['pic']) #設定變數來隨機儲存其中一個目標檔案路徑
+    pic = discord.File(random_pic_path) #設定變數來儲存目標檔案
+    #pic = discord.File(jdata["pic"]) #設定變數並放入目標檔案路徑來儲存目標檔案
+    await ctx.send(file = pic) #傳送檔案
+
+@bot.command() #指令
+async def random_web_pic(ctx): 
+    random_pic_url = random.choice(jdata['pic_url']) #設定變數隨機儲存其中一個目標圖片的網址
+    await ctx.send(random_pic_url) #傳送網址
 
 bot.run(jdata["TOKEN"]) #利用json設定檔存取裡面的discord bot token(類似字典)
