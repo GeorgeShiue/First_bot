207cd9830fa9b687953f10574c6666fe0fd03422
GeorgeShiue <georgeshiue1130@gmail.com>
2023-01-30 15:03:36 +0800

    Update bot.py

--- bot.py
+++ bot.py
@@ -10,11 +10,11 @@ async def on_ready(): #機器人上線後會印出以下句子
 @bot.event #事件
 async def on_member_join(member): #member變數代表伺服器成員
     channel = bot.get_channel(1069466066759196783) #建立頻道變數並放入目標頻道id
-    await channel.send(f'{member} join!') #在目標頻道內發送訊息
+    await channel.send(f'sup {member}') #在目標頻道內發送訊息
 
 @bot.event #事件
 async def on_member_remove(member):
     channel = bot.get_channel(1069466066759196783)
-    await channel.send(f'{member} leave!')
+    await channel.send(f'adios {member}')
 
 bot.run("MTA2ODU1ODg5MzgzNzQ2MzU3Mg.GUu1Kb.KqLVZBYas5TAgGj1ywzysfzN1vT6Iit3-FQmdc") #discord bot token
